907bd7045595233e93008a327a42b665535f0e13
David Leach <davidleach724@gmail.com>
2021-09-12 15:40:04 -0600

    Add logo to nav component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import NavBar from '../NavBar/NavBar'
 import './App.css'
 
 class App extends Component {
 
   render() {
     return(
-      <h1>Test</h1>
+      <NavBar />
     )
   }
 }
