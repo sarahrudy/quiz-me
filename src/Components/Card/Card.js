6f633c98c29c04962332495fc01cf3157f5cb3e6
David Leach <davidleach724@gmail.com>
2021-09-15 20:56:42 -0600

    Add padding to answers

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -14,19 +14,19 @@ const Card = ({ question, length, position }) => {
       <p className="question">{info.question}</p>
      </div>
      <form className="answers-container">    
-        <div>
+        <div className="answers">
           <input type="radio" name="answer" value="a0"></input>
           <label htmlFor="a0">{info.answers[0]}</label>
         </div>
-        <div>
+        <div className="answers">
           <input type="radio" name="answer" value="a1"></input>
           <label htmlFor="a1">{info.answers[1]}</label>
         </div>
-        <div>
+        <div className="answers">
           <input type="radio" name="answer" value="a2"></input>
           <label htmlFor="a2">{info.answers[2]}</label>
         </div>
-        <div>
+        <div className="answers">
           <input type="radio" name="answer" value="a3"></input>
           <label htmlFor="a3">{info.answers[3]}</label>
         </div>
