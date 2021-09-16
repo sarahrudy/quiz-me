eb6c8b451559fb32a67a2ff07fb7d112f13e0eff
David Leach <davidleach724@gmail.com>
2021-09-16 10:42:53 -0600

    Add index as key for each radio answer

--- src/Components/Answers/Answers.js
+++ src/Components/Answers/Answers.js
@@ -3,16 +3,16 @@ import React from "react"
 const Answers = ({answerList}) => {
  return answerList.map((answer, i) => {
     return (
-      <div className="answers">
-      <label>
-        <input
-          type="radio"
-          name="answers"
-          value={`a${i}`}
-          className="form-check-input"
-        />
-        {answer}
-      </label>
+      <div className="answers" key={i}>
+        <label>
+          <input
+            type="radio"
+            name="answers"
+            value={`a${i}`}
+            className="form-check-input"
+          />
+          {answer}
+        </label>
       </div>
     )
   })
