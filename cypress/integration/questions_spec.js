b777d730197843530dd0624aea00cbeca5ad24d2
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-16 10:49:57 -0600

    Add medium and hard display tests

--- cypress/integration/questions_spec.js
+++ cypress/integration/questions_spec.js
@@ -8,5 +8,34 @@ it('should display questions if easy button is clicked', () => {
   cy.get('[href="/questions/easy"] > .button')
     .click()
   cy.get(':nth-child(1) > .top-info > h3').contains('Entertainment: Video Games')
-  cy.get(':nth-child(1) > .answers-container > :nth-child(4) > label').contains('September 1, 1986') 
+  cy.get(':nth-child(1) > .answers-container > :nth-child(4) > label').contains('September 1, 1986')
+})
+
+it('should display questions if the medium button is clicked', () => {
+  cy.getQuestions('medium')
+  cy.get('[href="/questions/medium"] > .button')
+    .click()
+  cy.get(':nth-child(2) > .top-info')
+    .contains('Entertainment: Books')
+  cy.get(':nth-child(2) > .answers-container > :nth-child(3) > label')
+    .contains('Leviticus')
+})
+
+
+it('should display questions if the hard button is clicked', () => {
+  cy.getQuestions('hard')
+  cy.get('[href="/questions/hard"] > .button')
+    .click()
+  cy.get(':nth-child(3) > .answers-container > :nth-child(1) > label')
+    .contains('Different Appearance')
+  cy.get(':nth-child(3) > .answers-container > :nth-child(4) > label')
+    .contains('Different Gender')
+})
+
+it('should format out regex to make text readable', () => {
+  cy.getQuestions('easy')
+  cy.get('[href="/questions/easy"] > .button')
+    .click()
+  cy.get(':nth-child(3) > .question')
+    .contains('How does the character Dragowizard, Qinus Axia\'s from the anime "Buddyfight" differ between the Japanese and English dubs?')
 })
