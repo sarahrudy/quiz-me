9536195cbe0e2d34de90a64efde4c2f2281ec7fb
Sarah <sarahrudy@gmail.com>
2021-09-19 21:09:52 -0600

    Add content to Results message

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,6 +1,16 @@
 import React, { useContext } from 'react'
+import '../Results/Results.css'
+import { DifficultyContext } from '../../context/DifficultyContextProvider'
 
 const Results = () => {
+  const difficulty = useContext(DifficultyContext)
+
+  return (
+    <div className="results-container">
+      <h1>YOU'RE SO SMART</h1>
+      <p>You got {difficulty.userScore[0]} out of {difficulty.userScore[1]} correct!</p>
+    </div>
+  )
 }
 
 export default Results 
