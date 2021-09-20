4fd349612c3842d73197e3648d3efb530919c237
Sarah <sarahrudy@gmail.com>
2021-09-20 14:36:01 -0600

    Add // eslint-disable-next-line react-hooks/exhaustive-deps as a work around

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -4,7 +4,6 @@ import '../Questions/Questions.css'
 import { fetchData } from '../../apiCalls'
 import { DifficultyContext } from '../../context/DifficultyContextProvider'
 import submitBtn from '../../Images/submit_btn.png'
-import { Link } from 'react-router-dom'
 
 const Questions = () => {
   const difficulty = useContext(DifficultyContext)
@@ -12,20 +11,8 @@ const Questions = () => {
   useEffect(() => {
     fetchData(difficulty.difficulty)
     .then(data => difficulty.setQuestions(data))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-  const validateAnswers = () => {
-    const scoreData = difficulty.correctAnswers.reduce((arr, elem, i) => {
-      arr[1] = difficulty.correctAnswers.length
-      if (elem === difficulty.submittedAnswers[`q${i}`]) {
-        arr[0]++
-      }
-
-      return arr
-    }, [0, 0])
-    
-    difficulty.submitUserScore(scoreData)
-  }
 
   const triviaQuestions = difficulty.questions.map(question => {
     return (
@@ -40,14 +27,11 @@ const Questions = () => {
 
   return(
     <div className="questions-container">
-        { triviaQuestions }
-        <div className="submit-container">
-    { Object.keys(difficulty.submittedAnswers).length === 10  && <Link to='/results'>
-          <img src= { submitBtn } className="submit-btn" alt="submit button"
-          onClick={() => validateAnswers()}></img>
-    </Link> }
-        </div>
+      { triviaQuestions }
+      <div className="submit-container">
+        <img src= { submitBtn } className="submit-btn" alt="submit button"></img>
       </div>
+    </div>
   )
 }
 
