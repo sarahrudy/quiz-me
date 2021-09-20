098ab95cc6100f4e8dd71693b646caef1501d63f
David Leach <davidleach724@gmail.com>
2021-09-20 11:22:48 -0600

    style result page

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,4 +1,7 @@
 import React, { useContext } from 'react'
+import playAgain from '../../Images/playagain_btn.png'
+import resultPrompt from '../../Images/resultprompt.png'
+import '../../Images/cloud.png'
 import '../Results/Results.css'
 import { DifficultyContext } from '../../context/DifficultyContextProvider'
 import { Link } from 'react-router-dom'
@@ -7,17 +10,13 @@ const Results = () => {
   const difficulty = useContext(DifficultyContext)
 
   return (
-    <article>
       <div className="results-container">
-      <h1>YOU'RE SO SMART</h1>
-      { difficulty.userScore && <p>You got {difficulty.userScore[0]} out of {difficulty.userScore[1]} correct!</p> }
+        <img src={ resultPrompt } className='result-prompt'></img>
+        <div>
+          { difficulty.userScore && <p className='user-prompt'>You got {difficulty.userScore[0]} out of {difficulty.userScore[1]} correct!</p> }
+        </div>
+        <Link to='/'><img src= { playAgain } className="play-again-btn"></img></Link>
       </div>
-      <div>
-      <Link to='/'> 
-      <button>PLAY AGAIN</button>
-      </Link>
-      </div>
-    </article>
   )
 }
 
