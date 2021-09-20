5b773af2cce03ac0923df0145ce15a3352df3ff5
Sarah <sarahrudy@gmail.com>
2021-09-20 09:53:19 -0600

    Add logic for Play Again  button on Results page

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,15 +1,23 @@
 import React, { useContext } from 'react'
 import '../Results/Results.css'
 import { DifficultyContext } from '../../context/DifficultyContextProvider'
+import { Link } from 'react-router-dom'
 
 const Results = () => {
   const difficulty = useContext(DifficultyContext)
 
   return (
-    <div className="results-container">
+    <article>
+      <div className="results-container">
       <h1>YOU'RE SO SMART</h1>
-      <p>You got {difficulty.userScore[0]} out of {difficulty.userScore[1]} correct!</p>
-    </div>
+      { difficulty.userScore && <p>You got {difficulty.userScore[0]} out of {difficulty.userScore[1]} correct!</p> }
+      </div>
+      <div>
+      <Link to='/'> 
+      <button>PLAY AGAIN</button>
+      </Link>
+      </div>
+    </article>
   )
 }
 
