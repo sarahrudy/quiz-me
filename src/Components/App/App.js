80b363c3c1231be02db88abe3405a4da8fcf17c5
Sarah <sarahrudy@gmail.com>
2021-09-20 15:30:19 -0600

    Add new Route and Switch for 404 page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import NavBar from '../NavBar/NavBar'
 import TriviaDifficulty from '../TriviaDifficulty/TriviaDifficulty'
 import Questions from '../Questions/Questions'
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import DifficultyContextProvider from '../../context/DifficultyContextProvider'
 import './App.css'
 import Results from '../Results/Results'
+import Error from '../Error/Error'
 
 class App extends Component {
 
@@ -14,11 +15,12 @@ class App extends Component {
       <main>
         <DifficultyContextProvider>
           <NavBar />
-          <section>
+          <Switch>
             <Route exact path='/' component= { TriviaDifficulty } />
             <Route exact path='/questions/:difficulty' component= { Questions }/>
             <Route exact path='/results' component= { Results } />
-          </section>
+            <Route component= { Error } />
+          </Switch>
         </DifficultyContextProvider>
       </main>
     )
